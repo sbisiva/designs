bd7bbe863a5d563fabcc3452609257218488597b
CEN-E397-23\Centizen_User <sivanraj@centizen.com>
2024-01-05 10:58:40 +0530

    space

--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -11,6 +11,7 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 @NgModule({
   declarations: [],
   imports: [
+    
     CommonModule,
     MatTableModule,
     MatButtonModule,
